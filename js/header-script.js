5f9741326e3931496e31fe0dbc7100bcf1a0b85a
gayan <kanushkanet@gmail.com>
2020-07-12 20:58:40 +0530

    add animation to nav bar add custom courses page

--- js/header-script.js
+++ js/header-script.js
@@ -1,6 +1,6 @@
 particlesJS("header-background", {
   particles: {
-    number: { value: 10, density: { enable: true, value_area: 800 } },
+    number: { value: 20, density: { enable: true, value_area: 800 } },
     color: { value: "#b6922e" },
     shape: {
       type: "polygon",
@@ -14,7 +14,7 @@ particlesJS("header-background", {
       anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
     },
     size: {
-      value: 63.131811330581804,
+      value: (screen.width/25),
       random: true,
       anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
     },
@@ -59,21 +59,3 @@ particlesJS("header-background", {
   },
   retina_detect: true,
 });
-
-// var count_particles, stats, update;
-// stats = new Stats();
-// stats.setMode(0);
-// stats.domElement.style.position = "absolute";
-// stats.domElement.style.left = "0px";
-// stats.domElement.style.top = "0px";
-// document.body.appendChild(stats.domElement);
-// count_particles = document.querySelector(".js-count-particles");
-// update = function () {
-//   stats.begin();
-//   stats.end();
-//   if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-//     count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-//   }
-//   requestAnimationFrame(update);
-// };
-// requestAnimationFrame(update);
--- js/init.js
+++ js/init.js
@@ -1,8 +1,22 @@
 (function($){
+
   $(function(){
 
     $('.sidenav').sidenav();
     $('.scrollspy').scrollSpy();
 
   }); // end of document ready
+
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-64px";
+    }
+    prevScrollpos = currentScrollPos;
+  }
+
 })(jQuery); // end of jQuery name space
+
