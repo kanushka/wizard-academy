8eb0f53380d2e993b6c9ba5b2ae12179726419f4
gayan <gayan@extrogene.com>
2020-07-23 20:07:37 +0530

    update styles and remove nav bar animation

--- js/init.js
+++ js/init.js
@@ -9,16 +9,16 @@
 
   var rellax = new Rellax('.rellax');
 
-  var prevScrollpos = window.pageYOffset;
-  window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      document.getElementById("navbar").style.top = "0";
-    } else {
-      document.getElementById("navbar").style.top = "-64px";
-    }
-    prevScrollpos = currentScrollPos;
-  }
+  // var prevScrollpos = window.pageYOffset;
+  // window.onscroll = function() {
+  //   var currentScrollPos = window.pageYOffset;
+  //   if (prevScrollpos > currentScrollPos) {
+  //     document.getElementById("navbar").style.top = "0";
+  //   } else {
+  //     document.getElementById("navbar").style.top = "-64px";
+  //   }
+  //   prevScrollpos = currentScrollPos;
+  // }
 
 })(jQuery); // end of jQuery name space
 
